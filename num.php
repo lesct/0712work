af49660835c0c6033cbb61c53e3466a50df8df8c
lesct <1940870357@qq.com>
2016-07-12 22:38:00 +0800

    number table num.php file

---
+++ num.php
@@ -0,0 +1,16 @@
+<?php
+
+for($i=0;$i<1000000;$i++)
+{
+	$uid = sprintf("%05d",$i);
+	echo $uid."\t";
+
+	if(rand(1,3)==1)
+	{
+		echo rand(30,59)."\n";
+	}
+	else
+	{
+		echo rand(60,100)."\n";
+	}
+}
